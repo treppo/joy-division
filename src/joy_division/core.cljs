20262248d89ee849b9abe2acadf52512a33c6599
Christian Treppo <christian@treppo.org>
2020-06-28 15:14:11 +0200

    Fix quadratic curve drawing

--- src/joy_division/core.cljs
+++ src/joy_division/core.cljs
@@ -11,12 +11,13 @@
                 a))
 (def frequency-ints (js/Uint8Array. (.-frequencyBinCount analyser)))
 
+(def max-amplitude (* (.-frequencyBinCount analyser) step))
+
 (def canvas
   (let [element (js/document.querySelector "canvas")
-        size (* (.-frequencyBinCount analyser) step)
         dpr js/devicePixelRatio]
-    (set! (.-width element) (* size dpr))
-    (set! (.-height element) (* size dpr))
+    (set! (.-width element) (* max-amplitude dpr))
+    (set! (.-height element) (* max-amplitude dpr))
     element))
 
 (def canvas-context
@@ -35,38 +36,43 @@
   (map (fn [i] (/ i 256)) ints))
 
 (defn to-points [frequencies]
-  (map-indexed (fn [i ratio] {:x (* i step), :y (* ratio (.-height canvas))}) frequencies))
+  (map-indexed (fn [i ratio] {:x (* i step), :y (* ratio max-amplitude)}) frequencies))
 
-(defn control-points [points]
-  (map (fn [point next-point] {:x (/ (+ (:x point) (:x next-point)) 2), :y (/ (+ (:y point) (:y next-point)) 2)}) points (rest points)))
+(defn offset [points] (map (fn [{x :x, y :y}] {:x (+ x (/ step 2)), :y y}) points))
 
-(defn clear-canvas []
-  (.clearRect canvas-context 0 0 (.-width canvas) (.-height canvas)))
+(defn clear-canvas [context]
+  (.clearRect context 0 0 (.-width (.-canvas context)) (.-height (.-canvas context))))
+
+(defn midway-points [points]
+  (let [midway-point (fn [point next]
+                       {:x (/ (+ (:x point) (:x next)) 2)
+                        :y (/ (+ (:y point) (:y next)) 2)})]
+    (map midway-point points (rest points))))
+
+(defn join-points [xs ys] (map (fn [x y] [x y]) xs ys))
 
 (defn draw [points]
-  (let [offset (/ step 2)]
-    (clear-canvas)
-    (.beginPath canvas-context)
-    (.moveTo canvas-context (+ offset (:x (first points))) (:y (first points)))
+  {:desc
+   "Drawing smooth lines between points by calculating midway-points,
+    drawing curves between those and using the points as control points"}
 
-    (doseq [point (butlast points)
-            next-point (rest points)]
-      (let [x (+ offset (:x point))
-            y (:y point)
-            cx (/ (+ x (:x next-point)) 2)
-            cy (/ (+ y (:y next-point)) 2)]
-        (.quadraticCurveTo canvas-context x y cx cy)))
+  (clear-canvas canvas-context)
+  (.beginPath canvas-context)
+  (.moveTo canvas-context (:x (first (midway-points points))) (:y (first (midway-points points))))
 
-    (.stroke canvas-context)))
+  (doseq [[point midway-point] (join-points (butlast (rest points)) (rest (midway-points points)))]
+    (.quadraticCurveTo canvas-context (:x point) (:y point) (:x midway-point) (:y midway-point)))
+
+  (.stroke canvas-context))
 
 (defn update-loop []
   (-> (update-frequencies!)
-      normalize
-      to-points
-      draw)
-  ;(js/console.log (clojure.string/join ", " (normalize (update-frequencies!))))
-
-  (js/requestAnimationFrame update-loop))
+       normalize
+       to-points
+       offset
+       draw)
+  (js/requestAnimationFrame update-loop)
+  )
 
 (go
   (let [stream (<p! (js/navigator.mediaDevices.getUserMedia #js {:audio true}))
