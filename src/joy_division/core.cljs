cbec66938449683a0bf2a1f689ec8333eeb2d525
Christian Treppo <christian@treppo.org>
2020-07-10 11:54:10 +0200

    Optimize loop

--- src/joy_division/core.cljs
+++ src/joy_division/core.cljs
@@ -2,7 +2,7 @@
   (:require [cljs.core.async :refer [go]]
             [cljs.core.async.interop :refer-macros [<p!]]))
 
-(enable-console-print!)
+;(enable-console-print!)
 
 (def lines-count 32)
 (def points-per-line 32)
@@ -16,7 +16,7 @@
                 a))
 
 (defn frequency-bin-count [analyser]
-  "half the fftSize of the analyser"
+  "half of the analyser's fftSize"
   (.-frequencyBinCount analyser))
 
 (def canvas-dimension (* points-per-line step js/devicePixelRatio))
@@ -37,63 +37,51 @@
     (set! (.-strokeStyle ctx) "white")
     ctx))
 
-(defn update-frequencies! []
+(defn updated-frequencies []
   (.getByteTimeDomainData analyser frequency-uint-array)
   (js/Array.prototype.slice.call frequency-uint-array))
-
-(defn normalize [ints]
-  (map (fn [i] (/ i 256)) ints))
-
-(defn amplify [frequencies]
-  (map (fn [frequency] (* 128 frequency)) frequencies))
-
-(defn to-points [line-index frequencies]
-  (map-indexed (fn [i frequency] {:x (* i step), :y (- 320 frequency (* line-index y-step))}) frequencies))
-
-(defn offset [points]
-  (map (fn [{x :x, y :y}] {:x (+ x (/ step 2)), :y y}) points))
 
 (defn clear [context]
   (.clearRect context 0 0 (.-width (.-canvas context)) (.-height (.-canvas context))))
 
-(defn midway-points [points]
-  (let [midway-point (fn [point next]
-                       {:x (/ (+ (:x point) (:x next)) 2)
-                        :y (/ (+ (:y point) (:y next)) 2)})]
-    (map midway-point points (rest points))))
-
-(defn join-points [xs ys] (map (fn [x y] [x y]) xs ys))
-
-(defn draw [lines]
-  "Drawing smooth lines between points by calculating midway-points,
-   drawing curves between those and using the points as control points"
-
+(defn draw-lines [lines]
   (clear canvas-context)
   (doseq [points lines]
     (.beginPath canvas-context)
-    (.moveTo canvas-context (:x (first (midway-points points))) (:y (first (midway-points points))))
+    (.moveTo canvas-context (:x (first points)) (:y (first points)))
 
-    (doseq [[point midway-point] (join-points (butlast (rest points)) (rest (midway-points points)))]
-      (.quadraticCurveTo canvas-context (:x point) (:y point) (:x midway-point) (:y midway-point)))
-    (.save canvas-context)
-    (.fill canvas-context)
-    (set! (.-globalCompositeOperation canvas-context) "destination-out")
-    (.restore canvas-context)
+    (doseq [point (rest points)]
+      (.lineTo canvas-context (:x point) (:y point)))
 
     (.stroke canvas-context)))
 
-(defn update-loop [previous]
-  (let [freqs (take lines-count (conj previous (update-frequencies!)))]
-    (->> (conj freqs)
-         (map normalize)
-         (map amplify)
-         (map-indexed to-points)
-         (map offset)
-         draw)
-    (js/requestAnimationFrame (fn [] (update-loop freqs)))))
+(defn normalize [integer] (/ integer 256))
+
+(defn amplify [frequency] (* 128 frequency))
+
+(defn point [i frequency]
+  (let [height (* y-step lines-count)]
+    {:x (* i step), :y (- height frequency)}))
+
+(defn x-align [{x :x, y :y}] {:x (+ x (/ step 2)), :y y})
+
+(def point-xf (comp (map normalize) (map amplify) (map-indexed point) (map x-align)))
+
+
+(defn vertical-shift [points] (map (fn [{x :x, y :y}] {:x x, :y (- y y-step)}) points))
+
+(defn shift-lines [lines] (map vertical-shift (take (- lines-count 1) lines)))
+
+(defn update-loop
+  ([] (update-loop ()))
+  ([previous]
+   (let [new-line (into () point-xf (updated-frequencies))
+         shifted (shift-lines previous)
+         lines (conj shifted new-line)]
+     (js/requestAnimationFrame (fn [] (draw-lines lines) (update-loop lines))))))
 
 (go
   (let [stream (<p! (js/navigator.mediaDevices.getUserMedia #js {:audio true}))
         source (.createMediaStreamSource audioContext stream)]
     (.connect source analyser)
-    (update-loop ())))
+    (update-loop)))
