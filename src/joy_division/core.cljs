3eca25239a5b64066b17615ed78d0419ec6db175
Christian Treppo <christian@treppo.org>
2021-04-07 12:00:38 +0200

    Calculate config dynamically

--- src/joy_division/core.cljs
+++ src/joy_division/core.cljs
@@ -4,24 +4,46 @@
     [cljs.core.async.interop :refer-macros [<p!]]))
 
 
-;; (enable-console-print!)
-
-(def lines-count 32)
 (def y-step 10)
 (def step 8)
-(def window-width (.-innerWidth js/window))
-
-
-(def resolution
-  "has to be power of two"
-  (let [fitting-exponent (Math/floor (/ (Math/log window-width) (Math/log 2)))]
-    (Math/pow 2 (- fitting-exponent 1))))                   ; decrement exponent to for the graph to be portrait
-(def points-per-line (/ resolution 2 step))
-(def width (* points-per-line step))
-(def height (+ 20 (* lines-count y-step)))
+(def top-padding 20)
+(def bottom-margin 20)
 (def amplification 20)
 (def margin 10)
 (def line-width 2)
+(defn window-width [] (.-innerWidth js/window))
+(defn window-height [] (.-innerHeight js/window))
+
+
+(defn resolution
+  []
+  "has to be power of two"
+  (let [fitting-exponent (Math/floor (/ (Math/log (window-width)) (Math/log 2)))]
+    ;; decrement exponent for the graph to be portrait
+    (Math/pow 2 (- fitting-exponent 1))))
+
+
+(defn points-per-line [] (/ (resolution) js/devicePixelRatio step))
+(defn width [] (* (points-per-line) step))
+
+
+(defn lines-count
+  []
+  (/ (Math/floor (/ (- (window-height) (* js/devicePixelRatio top-padding) bottom-margin) y-step)) js/devicePixelRatio))
+
+
+(defn height [] (+ top-padding (* (lines-count) y-step)))
+
+
+(defn init-config
+  []
+  {:y-step y-step
+   :step step
+   :width (width)
+   :height (height)
+   :margin margin
+   :amplification amplification
+   :lines-count (lines-count)})
 
 
 (defn create-audio-context
@@ -34,19 +56,21 @@
 (defn create-analyser
   [audio-context]
   (let [a (.createAnalyser audio-context)]
-    (set! (.-fftSize a) resolution)
+    (set! (.-fftSize a) (resolution))
     a))
 
 
-(def canvas
+(defn canvas
+  [{:keys [width height]}]
   (let [element (js/document.querySelector "canvas")]
     (set! (.-width element) (* width js/devicePixelRatio))
     (set! (.-height element) (* height js/devicePixelRatio))
     element))
 
 
-(def canvas-context
-  (let [ctx (.getContext canvas "2d")
+(defn canvas-context
+  [config]
+  (let [ctx (.getContext (canvas config) "2d")
         dpr js/devicePixelRatio]
     (.scale ctx dpr dpr)
     (set! (.-lineWidth ctx) line-width)
@@ -67,7 +91,7 @@
 
 
 (defn draw-lines
-  [lines]
+  [lines canvas-context]
   (clear canvas-context)
   (doseq [[first-point & rest-points] lines]
     (.beginPath canvas-context)
@@ -93,7 +117,7 @@
 
 
 (defn shaped
-  [i frequency]
+  [i frequency {:keys [step width margin amplification]}]
   (let [x (* step i)
         distance-to-center (Math/abs (- x (/ width 2)))
         variance (- (/ width 2) distance-to-center)
@@ -102,30 +126,31 @@
 
 
 (defn point
-  [i frequency]
+  [i frequency {step :step height :height}]
   (Point. (* step i) (- height frequency)))
 
 
 (defn x-align
-  [point]
+  [point step]
   (Point. (+ (.-x point) (/ step 2)) (.-y point)))
 
 
 (defn when-at-step
-  [index point]
-  (if (zero? (mod index step)) point))
+  [index frequency step]
+  (if (zero? (mod index step)) frequency))
 
 
-(def point-xf
-  (comp (keep-indexed when-at-step)
+(defn point-xf
+  [config]
+  (comp (keep-indexed (fn [idx freq] (when-at-step idx freq (:step config))))
         (map normalized)
-        (map-indexed shaped)
-        (map-indexed point)
-        (map x-align)))
+        (map-indexed (fn [idx freq] (shaped idx freq config)))
+        (map-indexed (fn [idx freq] (point idx freq config)))
+        (map (fn [point] (x-align point (:step config))))))
 
 
 (defn shift-line
-  [points]
+  [points y-step]
   (loop [remaining points
          acc (transient [])]
     (if (empty? remaining)
@@ -137,19 +162,20 @@
 
 
 (defn shift-lines
-  [lines]
-  (map shift-line (take (- lines-count 1) lines)))
+  [lines {:keys [y-step lines-count]}]
+  (map #(shift-line % y-step) (take (- lines-count 1) lines)))
 
 
 (defn update-loop
   ([frequencies-fn] (js/requestAnimationFrame (fn [] (update-loop [] frequencies-fn))))
   ([previous frequencies-fn]
-   (let [new-line (into () point-xf (frequencies-fn))
-         shifted (shift-lines previous)
+   (let [config (init-config)
+         new-line (into () (point-xf config) (frequencies-fn))
+         shifted (shift-lines previous config)
          lines (conj shifted new-line)]
      (js/requestAnimationFrame
        (fn []
-         (draw-lines (reverse lines))
+         (draw-lines (reverse lines) (canvas-context config))
          (update-loop lines frequencies-fn))))))
 
 
@@ -161,7 +187,7 @@
           source (.createMediaStreamSource audio-context stream)
           analyser (create-analyser audio-context)]
       (.connect source analyser)
-      (update-loop (fn [] (updated-frequencies analyser))))))
+      (update-loop #(updated-frequencies analyser)))))
 
 
 (defn main
